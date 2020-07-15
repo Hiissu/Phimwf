626d98b08c39c0828ed390a4f0d1ffa86bada075
Hiissu <44549729+Hiissu@users.noreply.github.com>
2020-07-15 14:10:40 +0700

    ...

--- PhimWFA/SQLQuery.sql
+++ PhimWFA/SQLQuery.sql
@@ -1,109 +1,91 @@
-create database QLyPhim
-
-drop table Phim
-drop table NhanVien
-drop table Rap
-drop table TTCT_Phim
-drop table TTCT_Rap
-
+create database dbQlyPhim
 
 create table Phim
 (
-ma nvarchar(100) primary key not null,
-ten nvarchar(100),
-theloai nvarchar(100),
-quocgia nvarchar(100),
-ngaychieu nvarchar(100),
-diemdanhgia float
+id nvarchar(20) primary key not null,
+ten nvarchar(50) not null,
+theloai nvarchar(50) not null,
+quocgia nvarchar(50) not null,
+diemdanhgia float,
 )
 
-delete from Phim
+insert into Phim values ('P01', 'Phim01', 'Hanhdong', 'My', 6.5)
+insert into Phim values ('P02', 'Phim02', 'Hai', 'Anh', 6.4)
+insert into Phim values ('P03', 'Phim03', 'Kinhdi', 'Phap', 8.2)
 
-insert into Phim values ('P01', 'Phim A', 'Hanh dong', 'Anh', '11/01/2010', 9.5)
-insert into Phim values ('P02', 'Phim A', 'Hanh dong', 'Anh', '11/01/2010', 9.5)
-insert into Phim values ('P03', 'Phim A', 'Hanh dong', 'Anh', '11/01/2010', 9.5)
-insert into Phim values ('P04', 'Phim A', 'Hanh dong', 'Anh', '11/01/2010', 9.5)
-insert into Phim values ('P05', 'Phim A', 'Hanh dong', 'Anh', '11/01/2010', 9.5)
-
-
-create table NhanVien
+create table Nhanvien
 (
-ma nvarchar(100) primary key not null,
-ten nvarchar(100),
-ngaysinh nvarchar(100),
-noisinh nvarchar(100),
-chucvu nvarchar(100)
+id nvarchar(20) primary key not null,
+ten nvarchar(50) not null,
+chucvu nvarchar(50) not null,
 )
 
-insert into NhanVien values ('NV01', 'Nguyen A', '01/01/2010', 'My', 'Dao dien')
-insert into NhanVien values ('NV02', 'Nguyen W', '01/01/2010', 'My', 'Dao dien')
-insert into NhanVien values ('NV03', 'Nguyen AS', '01/01/2010', 'My', 'Dien vien')
-insert into NhanVien values ('NV04', 'Nguyen D', '01/01/2010', 'My', 'Dien vien')
-insert into NhanVien values ('NV05', 'Nguyen B', '01/01/2010', 'My', 'Dien vien')
+insert into Nhanvien values ('NV01', 'Harry', 'Dienvien')
+insert into Nhanvien values ('NV02', 'Lucas', 'Daodien')
+insert into Nhanvien values ('NV03', 'Alex', 'Dienvien')
+insert into Nhanvien values ('NV04', 'Ana', 'Dienvien')
+insert into Nhanvien values ('NV05', 'Lucy', 'Dienvien')
+insert into Nhanvien values ('NV06', 'Peter', 'Dienvien')
 
+create table TTPhim
+(
+id_phim nvarchar(20) not null,
+id_nhanvien nvarchar(20)not null,
+primary key (id_phim, id_nhanvien),
+foreign key (id_phim) references Phim(id),
+foreign key (id_nhanvien) references Nhanvien(id)
+)
+
+
+insert into TTPhim values ('P01', 'NV01')
+insert into TTPhim values ('P01', 'NV02')
+insert into TTPhim values ('P01', 'NV03')
+insert into TTPhim values ('P01', 'NV06')
+
+insert into TTPhim values ('P02', 'NV06')
+insert into TTPhim values ('P02', 'NV01')
+insert into TTPhim values ('P02', 'NV04')
+insert into TTPhim values ('P02', 'NV05')
 
 create table Rap
 (
-ma nvarchar(100) primary key not null,
-ten nvarchar(100),
-diachi nvarchar(100)
+id nvarchar(20) primary key not null,
+ten nvarchar(50) not null,
+diachi nvarchar(50) not null,
 )
 
-insert into Rap values ('R01', 'Rap A', 'Ha Noi')
-insert into Rap values ('R02', 'Rap S', 'Ha Noi')
-insert into Rap values ('R03', 'Rap F', 'Ha Noi')
-insert into Rap values ('R04', 'Rap R', 'Ha Noi')
-insert into Rap values ('R05', 'Rap T', 'Ha Noi')
+insert into Rap values ('R01', 'CGV', 'Hanoi')
+insert into Rap values ('R02', 'Lotte', 'Hanoi')
+insert into Rap values ('R03', 'Galaxy', 'Hanoi')
 
 
-create table TTCT_Phim
+create table TTRap
 (
-id int identity(1, 1) primary key not null,
-ma_phim nvarchar(100),
-ma_nhanvien nvarchar(100),
-foreign key (ma_phim) references Phim(ma),
-foreign key (ma_nhanvien) references NhanVien(ma)
+id_phim nvarchar(20) not null,
+id_rap nvarchar(20) not null,
+primary key (id_phim, id_rap),
+foreign key (id_phim) references Phim(id),
+foreign key (id_rap) references Rap(id)
 )
 
-insert into TTCT_Phim(ma_phim, ma_nhanvien) values ('P01', 'NV01')
-insert into TTCT_Phim(ma_phim, ma_nhanvien) values ('P01', 'NV02')
-insert into TTCT_Phim(ma_phim, ma_nhanvien) values ('P01', 'NV03')
-insert into TTCT_Phim(ma_phim, ma_nhanvien) values ('P01', 'NV04')
-insert into TTCT_Phim(ma_phim, ma_nhanvien) values ('P01', 'NV05')
 
-create table TTCT_Rap
-(
-id int identity(1, 1) primary key not null,
-ma_phim nvarchar(100) not null,
-ma_rap nvarchar(100) not null,
-foreign key (ma_phim) references Phim(ma),
-foreign key (ma_rap) references Rap(ma)
-)
-
-insert into TTCT_Rap(ma_phim, ma_rap) values ('P01', 'R01')
-insert into TTCT_Rap(ma_phim, ma_rap) values ('P02', 'R01')
-insert into TTCT_Rap(ma_phim, ma_rap) values ('P03', 'R01')
-insert into TTCT_Rap(ma_phim, ma_rap) values ('P04', 'R01')
-insert into TTCT_Rap(ma_phim, ma_rap) values ('P05', 'R01')
-insert into TTCT_Rap(ma_phim, ma_rap) values ('P01', 'R02')
-insert into TTCT_Rap(ma_phim, ma_rap) values ('P01', 'R02')
+insert into TTRap values ('P01', 'R01')
+insert into TTRap values ('P02', 'R01')
+insert into TTRap values ('P03', 'R01')
+insert into TTRap values ('P02', 'R02')
+insert into TTRap values ('P03', 'R03')
+insert into TTRap values ('P02', 'R03')
 
 
+SELECT dbo.Nhanvien.ten as ten_NV, dbo.Nhanvien.chucvu,
+		dbo.Phim.ten as ten_Phim, dbo.Phim.theloai, dbo.Phim.quocgia, dbo.Phim.diemdanhgia
+		FROM dbo.Phim INNER JOIN dbo.TTPhim ON dbo.Phim.id = dbo.TTPhim.id_phim 
+						INNER JOIN dbo.Nhanvien ON dbo.TTPhim.id_nhanvien  = dbo.Nhanvien.id 
+		 WHERE dbo.Phim.id = 'P01'
 
-select Phim.ten, Phim.theloai, Phim.quocgia, NhanVien.ten, NhanVien.chucvu, Rap.ten, Rap.diachi 
-from ( select Phim.ten, Phim.theloai, Phim.quocgia, NhanVien.ma, Rap.ma from Phim inner join TTCT_Phim on TTCT_Phim.ma_phim = 'P01' inner join TTCT_Rap on TTCT_Rap.ma_phim = 'P01' ) as P
-, (select NhanVien.ten, NhanVien.chucvu from  ), Rap  
-Phim.ten, Phim.theloai, Phim.quocgia in ( ) and
-NhanVien.ten, NhanVien.chucvu in ( select ten, chucvu from NhanVien inner join TTCT_Phim on NhanVien.ma = TTCT_Phim.ma_nhanvien and
-
-Rap.ten, Rap.diachi  in 
-( and 
-
-Rap inner join TTCT_Rap on Rap.ma = TTCT_Rap.ma_rap) 
-	
-
-
-
-select Phim.ten, Phim.quocgia from 
-Phim inner join TTCT_Phim on TTCT_Phim.ma_phim = Phim.ma inner join TTCT_Rap on TTCT_Rap.ma_phim = Phim.ma
-where Phim.ma = 'P01'
+SELECT dbo.Phim.ten as ten_Phim, dbo.Phim.theloai, dbo.Phim.quocgia, dbo.Phim.diemdanhgia,
+		dbo.Rap.ten as ten_Rap, dbo.Rap.diachi
+		FROM dbo.Phim INNER JOIN dbo.TTRap ON dbo.Phim.id = dbo.TTRap.id_phim 
+						INNER JOIN dbo.Rap ON dbo.TTRap.id_rap  = dbo.Rap.id 
+		 --WHERE dbo.Phim.id = 'P01'
+		 WHERE dbo.Rap.id = 'R01'
